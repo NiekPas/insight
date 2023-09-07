1624c18e9891453a40c0e76b28b2a61973dbbd94
Niek van de Pas <niek_pas@live.nl>
2023-09-07 19:13:28 +0200

    chore: remove commented import

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,3 @@
-// import { flow } from "lodash";
-
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process unless
